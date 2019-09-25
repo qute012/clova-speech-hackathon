31491c5716a68f444598e3e323914784a54d6ec1
Eunhyuk Shin <silvershine157@gmail.com>
2019-09-25 17:36:56 +0900

    new config: lstm_deepenc, fix rn_nsml.sh to receive config

--- run_nsml.sh
+++ run_nsml.sh
@@ -12,4 +12,6 @@ MAX_EPOCHS=30
 nsml run -g $GPU_SIZE -c $CPU_SIZE -d $DATASET -a "--batch_size $BATCH_SIZE --workers $WORKER_SIZE --use_attention --max_epochs $MAX_EPOCHS"
 NO_CONFIG
 
-nsml run -g 1 -c 4 -d "sr-hack-2019-dataset"
+# nsml run -g 1 -c 4 -d "sr-hack-2019-dataset" -a "--config config/legacy/cfg0/baseline.cfg0.json"
+echo nsml run -g 1 -c 4 -d "sr-hack-2019-dataset" -a "--config $1"
+nsml run -g 1 -c 4 -d "sr-hack-2019-dataset" -a "--config $1"
