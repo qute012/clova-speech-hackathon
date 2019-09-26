3bfe0415f5df83c8699e17e61536007b7ca36c49
Eunhyouk Shin <silvershine157@gmail.com>
2019-09-27 00:46:36 +0900

    training macros

---
+++ automated_test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+while true
+do
+	echo "Lets try testing!"
+	nsml submit team161/sr-hack-2019-dataset/176 model
+	sleep 1
+done
---
+++ try_run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+while true
+do
+	./run_nsml.sh config/cfg2/lstm_deepenc_small_reg1.cfg2.json
+done
