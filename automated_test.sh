32a011439b023d2021c3ce9176f2e876c2edda83
Eunhyouk Shin <silvershine157@gmail.com>
2019-09-29 12:45:50 +0900

    update gitignore

--- automated_test.sh
+++ automated_test.sh
@@ -5,6 +5,9 @@ while true
 do
 	echo "Lets try testing!"
 	#nsml submit team161/sr-hack-2019-dataset/590 best
-	nsml submit team161/sr-hack-2019-dataset/176 model
-	sleep 1
+	nsml submit team161/sr-hack-2019-dataset/887 best
+	sleep 60
+	echo "Lets try testing!"
+	nsml submit team161/sr-hack-2019-dataset/886 best
+	sleep 60
 done
